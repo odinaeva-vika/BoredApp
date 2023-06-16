98e66c0d9966c9126468911ec51dbe974057e401
Victoria <121200782+odinaeva-vika@users.noreply.github.com>
2023-06-17 01:59:05 +0300

    init

---
+++ js-src/app.js
@@ -0,0 +1,6 @@
+//import "./modules/burger.js";
+//import "./modules/scroll.js";
+import "./modules/bored.js";
+
+import * as flsFunctions from "./modules/functions.js";
+flsFunctions.isWebp();
---
+++ js-src/modules/bored.js
@@ -0,0 +1,26 @@
+const buttonNode = document.querySelector(".button");
+
+const getButtonActive = () => {
+  getServerResponse();
+  renderTitle();
+  renderBodyClass();
+}
+
+const getServerResponse = () => {
+  fetch("http://www.boredapi.com/api/activity/")
+    .then (activity => activity.json())
+    .then ((res) => {
+      const text = res.activity;
+      document.querySelector(".text").innerText = `${text}`;
+  });
+}
+
+const renderTitle = () => {
+  document.querySelector(".title").innerText = "Ура, теперь не скучно 🔥";
+};
+
+const renderBodyClass = () => {
+  document.querySelector("body").style.background = "linear-gradient(180deg, rgba(0, 176, 28, 0.2) 24.48%, rgba(255, 255, 255, 0) 100%)";
+};
+
+buttonNode.addEventListener("click", getButtonActive);
